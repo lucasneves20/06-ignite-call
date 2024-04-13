223b2a29ace9c26331a902fc7ec3cd58f7f02899
webMasterAlumni <heraldofdevs@gmail.com>
2024-04-13 09:28:14 -0300

    avancei só um pouco

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,14 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Header } from './styles'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text></Text>
+
+      </Header>
+    </Container>
+  )
+}
