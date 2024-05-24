1343da40627b77f12ce2741c8cee68cdba1e812c
lucasneves20 <heraldofdevs.com>
2024-05-23 21:49:58 -0300

    terminei o módulo do calendário e disponibilidades

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -1,9 +1,7 @@
-/* eslint-disable camelcase */
-import { prisma } from '@/lib/prisma'
-/* import dayjs from 'dayjs' */
 import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../../lib/prisma'
 
-export default async function handle(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
@@ -15,7 +13,7 @@ export default async function handle(
   const { year, month } = req.query
 
   if (!year || !month) {
-    return res.status(400).json({ message: 'Year or month not provided.' })
+    return res.status(400).json({ message: 'Year or month not specified.' })
   }
 
   const user = await prisma.user.findUnique({
@@ -43,13 +41,29 @@ export default async function handle(
     )
   })
 
-  const blockedDateRaw = await prisma.$queryRaw`
-    SELECT *
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+    SELECT
+      EXTRACT(DAY FROM S.DATE) AS date,
+      COUNT(S.date) AS amount,
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+
     FROM schedulings S
+
+    LEFT JOIN user_time_intervals UTI
+      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
 
     WHERE S.user_id = ${user.id}
       AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+
+    GROUP BY EXTRACT(DAY FROM S.DATE),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+    HAVING amount >= size
   `
 
-  return res.json({ blockedWeekDays, blockedDateRaw })
+  const blockedDates = blockedDatesRaw.map((item) => item.date)
+
+  console.log(blockedDates)
+
+  return res.json({ blockedWeekDays, blockedDates })
 }
